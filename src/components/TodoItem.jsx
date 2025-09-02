4b5fe43f35c597618fe65508373d53982004518c
Francis james <ibolokefrancisjames2@gmail.com>
2025-09-02 13:54:41 +0100

    added react-icon for completed state

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { useTodo } from '../context/todoContext'
 import { Link } from 'react-router-dom'
+import { GoHeart, GoHeartFill } from "react-icons/go";
+
+
 
 const TodoItem = ({item}) => {
   const {deleteTodo, toggleCompleted} = useTodo()
@@ -15,14 +18,17 @@ const TodoItem = ({item}) => {
             }}
           >
             <div>
-              <h3>{item.title}</h3>
-              <p>{item.description}</p>
-              <div>
-                <button
+              <h3 className='font-bold mb-2'>Title: {item.title}</h3>
+              <p className='mb-4'><span>Description: </span>{item.description}</p>
+              <div className='flex gap-4 items-center'>
+                <div
                   onClick={() => toggleCompleted(item.id, item.isCompleted)}
+                  data-tooltip="Toggle completed task"
+                  title="Toggle completed task"
+                  aria-labelledby='button'
                 >
-                  {item.isCompleted ? "undo task" : "complete task"}
-                </button>
+                  {item.isCompleted ? <GoHeartFill color="red" size={25}/> : <GoHeart size={25}/>}
+                </div>
                 <Link
                   to={`:${item.id}`} 
                   style={{padding: "0.5rem 1rem", marginRight: "0.5rem"}}
